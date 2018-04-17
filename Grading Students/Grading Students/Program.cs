879f06578b60f8af09c5d5ae452bce958410cec2
Orkhan Farajov <orkhankhf@code.edu.az>
2018-04-17 10:38:37 +0400

    Problem Solved.

---
+++ Grading Students/Grading Students/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+    static int[] gradingStudents(int[] grades)
+    {
+        for (int i = 0; i < grades.Count(); i++)
+        {
+            if((grades[i]+2) % 5 == 0 && grades[i] > 37)
+            {
+                grades[i] = grades[i] + 2;
+            }
+        }
+        return grades;
+    }
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] grades = new int[n];
+
+        for (int gradesItr = 0; gradesItr < n; gradesItr++)
+        {
+            int gradesItem = Convert.ToInt32(Console.ReadLine());
+            grades[gradesItr] = gradesItem;
+        }
+
+        int[] result = gradingStudents(grades);
+
+        Console.WriteLine(string.Join("\n", result));
+    }
+}
